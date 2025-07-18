15c33caaaf62dcb5bf01a8621ed04f94207c8394
Sivaperumal B <sivaperumalb@Sivaperumals-MacBook-Air.local>
2025-07-18 22:17:39 +0530

    Updated portfolio content or design

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,14 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/Sivaperumal-portfolio/', // ✅ corrected base
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
